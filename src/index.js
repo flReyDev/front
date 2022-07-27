d9ec2fbd826ca9b7d68623dd5fa368b351596722
Leonardo <flrey@ucompensar.edu.co>
2022-07-26 20:53:16 -0500

    Modificación

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-import './css/index.css'
-import './style.css'
-import './css/container.css'
-import { toogleMenu } from "./helpers/help_navbar";
-import { header } from './components/Header';
-import { container } from './components/Container';
-
-let app = document.querySelector('#app');
-    app.innerHTML = header()
-    app.appendChild(container())
-    toogleMenu();
 
 
 
